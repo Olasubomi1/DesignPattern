1f0449013e20604e9ba26dcae5b6deda1e7e9a69
Olasubomi <shubbynlaa@gmail.com>
2023-03-11 11:44:00 +0100

    Refactored the canvas class to represent the CONTEXT class the state pattern uml.

--- src/State/Canvas.java
+++ src/State/Canvas.java
@@ -1,33 +1,21 @@
 package State;
 
 public class Canvas {
-    private ToolType currentTool;
+    private Tool currentTool;
 
     public void MouseDown(){
-        if(currentTool == ToolType.SELECTION){
-            System.out.println("Selection icon");
-        } else if (currentTool == ToolType.BRUSH) {
-            System.out.println("Brush icon");
-        }else if (currentTool == ToolType.ERASER){
-            System.out.println("Eraser icon");
-        }
+
     }
 
     public void MouseUp(){
-        if(currentTool == ToolType.SELECTION){
-            System.out.println("Selection: Draw dash rectangle");
-        } else if (currentTool == ToolType.BRUSH) {
-            System.out.println("Brush: Draw a line");
-        }else if (currentTool == ToolType.ERASER){
-            System.out.println("Eraser: Erase the line");
-        }
+
     }
 
-    public ToolType getCurrentTool() {
+    public Tool getCurrentTool() {
         return currentTool;
     }
 
-    public void setCurrentTool(ToolType currentTool) {
+    public void setCurrentTool(Tool currentTool) {
         this.currentTool = currentTool;
     }
 
